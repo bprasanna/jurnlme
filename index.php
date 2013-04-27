395f3e3b5c288bdee64c7d6fcae5414bf2ed52d6
bprasanna <prezna@gmail.com>
2013-04-28 01:07:32 +0530

    UI tweaks

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <title>Welcome</title>
 <body background-color:black>
 <?php 
-  echo "~~~~~~~jurnlme~~~~~~~~~~";
+  echo "jurnlme";
 ?>
-<hr>
+<hr style="color:gray">
 <form method="post" action"auth.php">
-<input type="text" name="username" placeholder="Username" />
+<input type="text" name="username" placeholder="Username" /><br>
 <input type="password" name="password" placeholder"Password" />
 <input type="submit" value="login" />
 </form>
