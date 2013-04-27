00a771f40b5cb106003d8d78e7402c6fadd73b65
bprasanna <prezna@gmail.com>
2013-04-28 01:05:16 +0530

    Adding login

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 <title>Welcome</title>
 <body background-color:black>
 <?php 
-  echo "My first app";
+  echo "~~~~~~~jurnlme~~~~~~~~~~";
 ?>
+<hr>
+<form method="post" action"auth.php">
+<input type="text" name="username" placeholder="Username" />
+<input type="password" name="password" placeholder"Password" />
+<input type="submit" value="login" />
+</form>
 </body>
 </html>
