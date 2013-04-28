7c31b1c76f30e993e260fe392a50a9d8c455bedc
bprasanna <prezna@gmail.com>
2013-04-28 23:05:07 +0530

    register

--- reg.php
+++ reg.php
@@ -9,10 +9,17 @@ $cache = new IronCache(array(
     'project_id' => '517d29c4ed3d762654000241'
 ));
 
+$user = $cache->get("username");
+if($user->value == $username) {
+  echo "User already exists. Please use other username";
+  header("Location: http://murmuring-inlet-9551.herokuapp.com/register.php"); /* Redirect browser */
+  exit();
+} else {
 $res = $cache->put("username", $username);
 $res = $cache->put("password", $password);
 $res = $cache->put("email", $email);
+echo "Successfully registered.<a href=\"index.php\">Login</a>";
+}
 
-echo $res;
 
 ?>
