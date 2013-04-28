a0fa4e3f3a4011cd670e4ee7e44221e0ca511c78
bprasanna <prezna@gmail.com>
2013-04-28 14:37:38 +0530

    Adding register

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr" itemscope itemtype="http://schema.org/Article">
 <head>
-<title>Welcome</title>
+<title>Register</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=yes">
 <link rel="stylesheet" type="text/css" media="all" href="decor.css" />
 <link rel="stylesheet" type="text/css" media="only screen and (max-width: 800px)" href="mob.css">
@@ -9,11 +9,12 @@
 <body>
 <h3>jurnlme</hr>
 <hr>
-<form method="post" action="auth.php">
+<form method="post" action="reg.php">
 <input type="text" name="username" placeholder="Username" /><br>
 <input type="password" name="password" placeholder="Password" /><br>
-<input type="submit" value="login" />
-<a href="register.php">Register</a>
+<input type="text" name="email" placeholder="Email" /><br>
+<input type="submit" value="register" />
+<a href="index.php">Login</a>
 </form>
 </body>
 </html>
