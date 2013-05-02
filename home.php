ed323d504e828d56933aca04a555d375b8e7754b
bprasanna <prezna@gmail.com>
2013-05-03 03:52:35 +0530

    session

--- home.php
+++ home.php
@@ -2,6 +2,8 @@
   if(!($_SESSION['un'])) {
     flush();
     header('Location: http://murmuring-inlet-9551.herokuapp.com/index.php');
+  } else {
+    echo $_SESSION['un'];
   }
 ?>
 <!DOCTYPE html>
@@ -22,7 +24,7 @@
 <hr>
 <h4>Recently...</h4>
 <?php
-$username = $_SESSION['un'];
+$usern = $_SESSION['un'];
 
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["HEROKU_POSTGRESQL_YELLOW_URL"]));
@@ -31,7 +33,7 @@ function pg_connection_string_from_database_url() {
 
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
-$result = pg_query($pg_conn, "select j.je from j20111988 j, t18982 u where t.uid=u.id and u.un='$username' order by ts desc");
+$result = pg_query($pg_conn, "select j.je from j20111988 j, t18982 u where t.uid=u.id and u.un='$usern' order by ts desc");
 
 if (!pg_num_rows($result)) {
   print("No notes added yet.\nFeel free add one anytime.");
