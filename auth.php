685c2414d213cd81deb743a7a477536f9f54dc88
bprasanna <prezna@gmail.com>
2013-05-03 03:10:41 +0530

    auth

---
+++ auth.php
@@ -0,0 +1,20 @@
+<?php
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["HEROKU_POSTGRESQL_YELLOW_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
+}
+
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$result = pg_query($pg_conn, "select un from t18982 where un='$username' and pa='$password'");
+
+if (!pg_num_rows($result)) {
+  print("Invalid Credentials. Please <a href=\"index.php\">Login</a> again");
+} else {
+  header('Location: http://murmuring-inlet-9551.herokuapp.com/jurnl.php');
+}
+
+?>
