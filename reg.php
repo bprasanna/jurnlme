79389d88473e3a347b2e23139f127f9fca9b5a45
bprasanna <prezna@gmail.com>
2013-05-03 02:15:31 +0530

    reg

--- reg.php
+++ reg.php
@@ -11,12 +11,15 @@ print "$host";
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
+echo $username;
+echo $password;
+echo $email;
 
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
 echo $pg_con;
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "select un from t18982 where un='$username'");
+$result = pg_query($pg_conn, "select un from t18982 where un ilike '$username'");
 
 if (!pg_num_rows($result)) {
   $result = pg_query($pg_conn, "insert into t18982 values('$username','$password','$email'");
