8e3d78231208baef028f72f310974be2a3e641a2
bprasanna <prezna@gmail.com>
2013-05-10 23:17:54 +0530

    date correct

--- load.php
+++ load.php
@@ -15,7 +15,7 @@ function pg_connection_string_from_database_url() {
 
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
-$result = pg_query($pg_conn, "select j.jid, j.je, to_char(j.ts,'DD-Mon-YYYY'), to_char(j.ts, 'HH24:MI:SS') from j20111988 j, t18982 u where j.uid=u.id and u.un='$usern' order by ts desc");
+$result = pg_query($pg_conn, "select j.jid, j.je, to_char(j.ts,'DD-Mon-YYYY'), to_char(j.ts, 'HH12:MI:SS AM') from j20111988 j, t18982 u where j.uid=u.id and u.un='$usern' order by ts desc");
 $out = "";
 
 if (!pg_num_rows($result)) {
@@ -24,11 +24,11 @@ if (!pg_num_rows($result)) {
    $pd = "";
    $out = $out."<article>";
    while ($row = pg_fetch_row($result)) { 
-         if (strcmp($row[2],$pd)>0){
+         if ($row[2] != $pd){
             $pd = $row[2];
             $out = $out."<b>$pd</b><br>";
          }
-         $out = $out."<span style=\"color:gray\">$row[3]</span>";
+         $out = $out." <span style=\"color:gray\">$row[3]</span>";
          $out = $out. "<section onclick=\"onf(this.id)\" id=$row[0] contenteditable=true onblur=\"ono(this.id)\">$row[1] </section>"; 
    }
    $out = $out."</article>";
