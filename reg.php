7d8b5b791bced8689db18beec6f544c558b46b7c
bprasanna <prezna@gmail.com>
2013-05-14 00:44:16 +0530

    reg

--- reg.php
+++ reg.php
@@ -14,7 +14,11 @@ $result = pg_query($pg_conn, "select un from t18982 where un ilike '$username'")
 
 if (!pg_num_rows($result)) {
   $result = pg_query($pg_conn, "insert into t18982(un,pa,em) values('$username','$password','$email')");
+  if($result>0){
   echo "Successfully registered.";
+  } else {
+  echo "Username already exists. Please select a different username.";
+  }
 } else {
   echo "Username already exists. Please select a different username.";
 }
